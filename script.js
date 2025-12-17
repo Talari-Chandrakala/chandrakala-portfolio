970250a160e05f4b063d432c08ec298ed8b8a4ea
Chinna9885 <guvvalamurthy@gmail.com>
2025-12-17 12:37:00 +0530

    Your commit message

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 document.querySelector('.download-btn').addEventListener('click', () => {
     alert('CV download started!');
 });
+
+// Hire Me button click handler
+document.querySelector('.hire-btn').addEventListener('click', () => {
+    window.open('https://mail.google.com/mail/?view=cm&fs=1&to=talarichandrakala2@gmail.com', '_blank');
+});
